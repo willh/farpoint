86d0622a8c2dd724ef735cf7cf946c9841c5d6fe
Will Hamill <william.hamill@gmail.com>
2018-01-02 23:12:11 +0000

    Adding workFactorMillis as config property to receiver sleep time
    By adding a longer [configurable] timeout for the message processing
    it is easier to simulate a failure during message processing by simply
    quitting the application during the thread sleep time. A sleep time of
    greater than 5000 milliseconds will more or less guarantee that any
    message being processed will not finish during the soft shutdown grace
    period, which makes for a more obvious test of the manual-ack.

--- farpoint-consumer/src/main/java/com/willhamill/farpoint/consumer/MessageReceiver.java
+++ farpoint-consumer/src/main/java/com/willhamill/farpoint/consumer/MessageReceiver.java
@@ -4,6 +4,7 @@ import com.rabbitmq.client.Channel;
 
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -11,13 +12,16 @@ import java.io.IOException;
 @Component
 public class MessageReceiver {
 
+    @Value("${workFactorMillis}")
+    private int workFactorMillis;
+
     @RabbitListener(queues = "${queueName}")
     public void receiveMessage(byte[] message, Message originalMessage, Channel channel) throws InterruptedException, IOException {
         System.out.println("Parsing message");
         String msg = new String(message);
 
-        // simulate doing some actual work
-        Thread.sleep(2000);
+        // simulate doing some actual work (anything taking longer than 5s will run beyond soft shutdown grace period)
+        Thread.sleep(workFactorMillis);
 
         System.out.println("Processed message: <"+ msg + ">");
 
